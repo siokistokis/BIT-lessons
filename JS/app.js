c929b622adb9008dca1a0ef2f9b361db13f0a095
siokistokis <siokis2000@yahoo.com>
2024-10-16 22:12:08 +0300

    changes

---
+++ JS/app.js
@@ -0,0 +1,13 @@
+
+console.log(hello);
+
+console.log(4+5*7);
+
+var myFirstNumber = 4;
+var mySecondNumber = 3.14;
+
+var myFirstAmount = myFirstNumber + mySecondNumber;
+
+var myFirstAmount = "hello";
+
+console.log(myFirstAmount);
