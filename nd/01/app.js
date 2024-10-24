bded3f7f3de4066ec751c0e3e37df9134f0f0498
siokistokis <siokis2000@yahoo.com>
2024-10-25 01:49:56 +0300

    changes

---
+++ nd/01/app.js
@@ -0,0 +1,20 @@
+console.log("Pradedam");
+
+//1
+
+const pTagH3 = document.querySelector('h3');
+
+pTagH3.innerText = 3
+
+//2
+const div1 = document.querySelector('div:nth-of-type(1)')
+const div2 = document.querySelector('div:nth-of-type(2)');
+
+div2.innerText = 1;
+div2.innerText = 2;
+
+//6 span find
+
+const span6 = document.querySelector('section#go6[data-amount] span');
+
+span6.innerText = 6
