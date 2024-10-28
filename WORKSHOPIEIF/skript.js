73b522dbb0fb3f373349f730b25e2fdf73b86b23
siokistokis <siokis2000@yahoo.com>
2024-10-28 15:19:58 +0200

    Changes

---
+++ WORKSHOPIEIF/skript.js
@@ -0,0 +1,17 @@
+const task1 = document.querySelector(".select1");
+const task2 = document.querySelector(".select2");
+const task3 = document.querySelector(".select3");
+const task4 = document.querySelector(".select4");
+const task5 = document.querySelector(".select5");
+const task6 = document.querySelector(".select6");
+const task7 = document.querySelector(".select7");
+const task8 = document.querySelector(".select8");
+const task9 = document.querySelector(".select9");
+const task10 = document.querySelector(".select10");
+
+const rand = function getRndIntegrel(min, max)
+{
+    returnMathf.flor(Math)
+}
+
+// Task1:Teigiami arba neigiami skaiciai
