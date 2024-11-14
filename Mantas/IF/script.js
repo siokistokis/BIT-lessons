06b973d87422c46b06715b56fdb8b617c4adee33
siokistokis <siokis2000@yahoo.com>
2024-11-15 00:49:17 +0200

    change

--- Mantas/IF/script.js
+++ Mantas/IF/script.js
@@ -50,138 +50,164 @@ const odd = numberss.filter(odddNr);
 //console.log(odd); 
 task3.innerHTML = (odd); 
 //4.
-// let arrayy = [-1, 32, 2, 3, 55, -4, 24, 5, -6, 7, -41, 8, 0, -9, -10, 11, 12, 78, -13, 69, -14, 15];
+let arrayy = [-1, 32, 2, 3, 55, -4, 24, 5, -6, 7, -41, 8, 0, -9, -10, 11, 12, 78, -13, 69, -14, 15];
+const oddint = (num) => num % 2 !== 0;
+const oddd = arrayy.filter(oddint);
+task4.innerHTML = (oddd);
+
 // for (let i = 0; i < numbers.length; i += 1) { 
 //     if (numbers[i] % 2 !== 0) { 
-//         console.log(i);
-//         arr.push(numbers[i]);
+//          console.log(i);
+//          arr.push(numbers[i]);
 //     } }
-// task4.innerHTML = (arrayy)
-//?????????????????????????????????????????????????????????????????????????
+//  task4.innerHTML = (arrayy)
+ 
 //5. 
 // Žodžių, ilgesnių nei 5 raidės, spausdinimas
 // Sukurkite sąrašą žodžių. Išspausdinkite tik tuos žodžius, kurių ilgis yra didesnis nei 5 raidės.
 let words = ["apple", "banana", "kiwi", "watermelon", "pear"];
 let arrT5 = []
 for (let i = 0; i < words.length; i++) {
+     //let words = `<div style= "word-spacing: 5px;">`
     if(words[i].length > 5) {
-        task5.innerHTML = words[i];
+        //task5.innerHTML = words[i];
         arrT5.push(words[i])
     }
+     // words += `</div>` 
     console.log(words[i])
 } task5.innerHTML = arrT5   
+   //task5.innerHTML = words[i]
 
-//???????????????????????????????????????????????????????????????
 //6.
 //my exemple:
-let wordss = ["bear", "wolf", "snake", "koala", "giraffe", "Deer", "cat", "leopatd", "Frog", "squirrel"]
-//for (let i = 0; i < wordss.length; i++) {
-//    if(wordss[i].length < 4) {
-//        console.log(wordss[i]);}}
-
-let min = wordss[0]; 
-for (const letters of wordss) { 
-    if (letters.length < min.length) { min = letters;} 
-    task6.innerHTML = min
-} 
+let wordss = ["bear", "wolf", "snake", "koala", "giraffe", "Deer", "cat", "leopard", "Frog", "squirrel"]
+let arrT6 = []
+for (let i = 0; i < wordss.length; i++) {
+    if(wordss[i].length < 5) {
+        arrT6.push(wordss[i])
+        console.log(wordss[i]);
+       
+    }
+}task6.innerHTML = arrT6;
+    //task6.innerHTML = wordss[i];
+// let min = wordss[0]; 
+// for (const letters of wordss) { 
+//     if (letters.length < min.length) { min = letters;} 
+//     task6.innerHTML = min
+// } 
 //console.log(min);
-//????????????????????????????????????????????????????????????????????
-
 
 //7. 
 // Skaičių dalinimas iš 3 be liekanos
 // Sukurkite sąrašą skaičių nuo 1 iki 20. Išrinkite ir išspausdinkite tik tuos skaičius, kurie dalijasi iš 3 be liekanos.
 let numbersArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
-
+let arrT7 = []
 for (let i =0; i < numbersArr.length; i++) {
     if(numbersArr[i] % 3 === 0) {
         //console.log(numbersArr[i]);
-         task7.innerHTML = numbersArr[i]; //??????????????????????????????????
-    }  
-}
+        arrT7.push(numbersArr[i])
+         //task7.innerHTML = numbersArr[i];
+    } 
+}  task7.innerHTML = arrT7
 //8.
 //my exemple:
+let arrT8 = []
 for (let i =0; i < numbersArr.length; i++) {
     if(numbersArr[i] % 5 === 0) {
         //console.log(numbersArr[i]);
-        task8.innerHTML = numbersArr[i];//?????????????????????????????????????
+        //task8.innerHTML = numbersArr[i];
+        arrT8.push(numbersArr[i])
     }
-}
+}  task8.innerHTML = arrT8
 
 //9.
 // Neigiamų skaičių suradimas sąraše
 // Sukurkite sąrašą teigiamų ir neigiamų skaičių. Išspausdinkite tik neigiamus skaičius.
 let negativeNumbers = [4, -3, 2, -1, 0, -5, 8, -9];
-
+let arrT9 = []
 for (let i = 0; i < negativeNumbers.length; i++) {
     if (negativeNumbers[i] < 0) {
         //console.log(negativeNumbers[i]);
-        task9.innerHTML = negativeNumbers[i];//?????????????????????????????????
+        //task9.innerHTML = negativeNumbers[i];
+        arrT9.push(negativeNumbers[i])
     }
-}
+}  task9.innerHTML = arrT9
  
 //10.
 // Teksto paieška sąraše
 // Sukurkite sąrašą sakinių. Išspausdinkite tik tuos sakinius, kurie turi žodį „JavaScript“.
 let sentences = ["I love programming", "JavaScript is awesome", "Coding is fun", "Learn JavaScript"];
+let arrT10 = []
 for (let i = 0; i < sentences.length; i++) {
     if(sentences[i].includes('JavaScript')) {
         //console.log(sentences[i]);
-        task10.innerHTML = sentences[i]; //???????????????????????????????????
+        arrT10.push(sentences[i])
+        //task10.innerHTML = sentences[i]; 
     }
-} 
+} task10.innerHTML = arrT10
+
 //11.
 //my exemple:
+let arrT11 = []
 for (let i = 0; i < sentences.length; i++) {
     if(sentences[i].includes('is')) {
         //console.log(sentences[i]);
-        task11.innerHTML = sentences[i];//??????????????????????????????????????
+        //task11.innerHTML = sentences[i];
+        arrT11.push(sentences[i])
     }
-} 
+} task11.innerHTML = arrT11
+
 //12.
 // Skaičių kvadratų suradimas
 // Sukurkite sąrašą skaičių. Išspausdinkite tik tuos skaičius, kurių kvadratas yra didesnis nei 50.
-let Numbers = [2, 5, 7, 8, 10, 12];
+let Numbers = [2, 5, 7, 8, 10, 12]
+let arrT12 = []
 for (let i = 0; i < Numbers.length; i++) {
     const Square = Numbers[i] **2;
     if(Square > 50) { 
          //console.log(Square);
-         task12.innerHTML = Numbers[i];//????????????????????????????????
+         //task12.innerHTML = Numbers[i];
+         arrT12.push(Numbers[i])
     }  
-}
+}task12.innerHTML = arrT12
 //13.
 //my exemple:
+let arrT13 = []
 for (let i = 0; i < Numbers.length; i++) {
     const Square = Numbers[i] **2;
     if(Square < 50) { 
-         console.log(Square);
-         task13.innerHTML = Numbers[i];
+        //  console.log(Square);
+        //  task13.innerHTML = Numbers[i];
+        arrT13.push(Numbers[i]);
     }  
-}
+}task13.innerHTML = arrT13
 
 //14.
 // Didžiausio skaičiaus paieška sąraše
 // Sukurkite sąrašą skaičių ir raskite didžiausią skaičių. Tikrinkite kiekvieną reikšmę cikle.
-let integers = [3, 45, 23, 67, 12, 90, 34]; 
-//didnotunderstand
+let integers = [3, 45, -14, 23, 67, -5, 12, 90, -37, 34]; 
+let arrT14 = []
 let theLargest = integers[0]; 
 for (let i=0; i < integers.length; i++) { 
 if (integers[i] > theLargest){
     theLargest = integers[i]; 
-    task14.innerHTML = theLargest; //???????????????????????????????
+    arrT14.push(integers[i]);
+    task14.innerHTML = theLargest; 
 } 
 //console.log(theLargest);
-} 
+} task14.innerHTML = arrT14
 //15.
 //my exemple:
+let arrT15 = []
 let theSmolest = integers[0]; 
 for (let i=0; i < integers.length; i++) { 
 if (integers[i] < theSmolest){
-    theSmolest = integers[i]; 
-    task15.innerHTML = theSmolest;//?????????????????????????????????????
+    theSmolest = integers[i];
+    arrT15.push(integers[i]);
+    task15.innerHTML = theSmolest;
 } 
-console.log(theSmolest);
-} 
+//console.log(theSmolest);
+} task15.innerHTML = arrT15
 //16.
 // Raidžių skaičiavimas žodyje
 // Sukurkite žodį ir suskaičiuokite, kiek kartų žodyje pasikartoja raidė „a“.
@@ -208,40 +234,47 @@ for (let i = 0; i < word.length; i++) {
 //18. 
 // Amžių filtravimas sąraše
 // Sukurkite sąrašą žmonių amžių. Išspausdinkite tik tuos, kurie yra vyresni nei 18.
-let ages = [12, 17, 22, 19, 15, 30, 18];
+let ages = [12, 10, 17, 40, 22, 36, 19, 15, 9, 30, 18];
+let arrT18 = []
 for (let i = 0; i < ages.length; i++) {
-    if(ages[i] > 18){
-         console.log(ages[i]);
-         task18.innerHTML = ages[i];//?????????????????????????????????????????
+    if(ages[i] > 18) {
+        console.log(ages[i]);
+        task18.innerHTML = ages[i];
+        arrT18.push(ages[i]);
     }  
-}
+}task18.innerHTML = arrT18
 //19.
 //my example:
+let arrT19 = []
 for (let i = 0; i < ages.length; i++) {
     if(ages[i] < 17){
-         console.log(ages[i]);
-         task19.innerHTML = ages[i]; //???????????????????????????????????????
+        console.log(ages[i]);
+        task19.innerHTML = ages[i];
+         arrT19.push(ages[i]);
     }  
-}
+}task19.innerHTML = arrT19
 //20. 
 // Skaičių dalybos liekanos tikrinimas
 // Sukurkite sąrašą skaičių. Išspausdinkite tik tuos skaičius, kurie, dalijant iš 2, turi liekaną.
-let oddNr = [5, 8, 13, 22, 31, 40];
-for (let i = 0; i < oddNr.length; i++) {
-    if(oddNr[i] % 2 !== 0) {
-        console.log(oddNr[i]);
-        task20.innerHTML = oddNr[i]; //????????????????????????????????????????/
+let justNr = [-14, -6, -1, -7, 5, 8, 11, 13, 22, 27, 31, 40];
+let oddNr = []
+for (let i = 0; i < justNr.length; i++) {
+    if(justNr[i] % 2 !== 0) {
+        console.log(justNr[i]);
+        task20.innerHTML = justNr[i]; 
+        oddNr.push(justNr[i])
     }
-}
+}task20.innerHTML = oddNr
 //21.
 //my example:
-let evenNr = [5, 8, 13, 22, 31, 40];
-for (let i = 0; i < evenNr.length; i++) {
-    if(evenNr[i] % 2 === 0) {
-        console.log(evenNr[i]);
-        task21.innerHTML = evenNr[i];//??????????????????????????????????
+let evenNr = [];
+for (let i = 0; i < justNr.length; i++) {
+    if(justNr[i] % 2 === 0) {
+        console.log(justNr[i]);
+        task21.innerHTML = justNr[i];
+        evenNr.push(justNr[i])
     }
-}
+}task21.innerHTML = evenNr
 
 
 //Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
