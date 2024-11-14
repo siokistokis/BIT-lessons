6511107c988eb6337bfdac4ceefc8ec278262245
siokistokis <siokis2000@yahoo.com>
2024-11-14 10:45:45 +0200

    changes

---
+++ Counter app/script.js
@@ -0,0 +1,19 @@
+const count = document.querySelector(".count")
+const subtract = document.querySelector(".subtract")
+const reset = document.querySelector(".reset")
+const add = document.querySelector(".add")
+
+add.addEventListener("click",function(){
+    count.innerHTML++
+})
+subtract.addEventListener("click",function(){
+    count.innerHTML--
+})
+
+function querySelector(){
+    for (let i = 0; i < 0; i++) {
+        if(????? i> 0) {
+           
+        }else difrent color
+    }
+}
