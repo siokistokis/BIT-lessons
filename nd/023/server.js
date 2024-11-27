0f3b49b91570ece85c68d4624af3d5c91071c1fe
siokistokis <siokis2000@yahoo.com>
2024-11-27 19:33:00 +0200

    changes

--- nd/023/server.js
+++ nd/023/server.js
@@ -5,9 +5,29 @@ const port = 3000;
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.send('Hello Deer!')
+  res.send('<h1>Hello!</h1>')
 });
 
+app.get('/beaver', (req, res) => {
+    res.send('<h1>Hello, Beaver!</h1>');
+  });
+
+  app.get('/beaver/:color/:size', (req, res) => {
+    const {color, size} = req.params;
+    // const color = req.params.color;
+    // const size = req.params.size;
+    res.send(`<h1 style="color: ${color}; font-size ${size}px;">Hello, Beaver!</h1>`);
+  });
+
+  app.get('/deer', (req, res) => {
+    const {color, size} = req.query;
+    res.send(`<h1 style="color: ${color ?? 'black'}; font-size ${size ?? 20}px;">Hello, Deer!</h1>`);
+  });
+
+  app.post('/form', (req, res) => {
+    res.send('Form post received!');
+  });
+
 app.listen(port, () => {
-  console.log(`Bebras darbui pasiruoses ant ${port} porto!`)
+  console.log(`Bebras darbui pasiruoses ant ${port} porto!`);
 });
