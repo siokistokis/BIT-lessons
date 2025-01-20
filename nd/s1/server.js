c39108924a884b72d5823e07c8655bb0966b0f79
siokistokis <siokis2000@yahoo.com>
2025-01-20 20:15:23 +0200

    changes

---
+++ nd/s1/server.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const app = express();
+const cors = require('cors');
+
+const port = 3333;
+app.use(bodyParser.json());
+
+app.use(cors());
+
+
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'cosmos'
+});
+
+app.get('/api/planet', (req, res) => {
+
+    const sql = `
+        SELECT * FROM planets
+    `;
+
+    con.query(sql, (err, result) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+
+        result.forEach(planet => {
+            planet.satellites = JSON.parse(planet.satellites);
+        });
+
+        res.json(result);
+    });
+
+});
+
+app.post('/api/planet', (req, res) => {
+
+    const { name, state, size, satellites } = req.body;
+
+    const sql = `
+        INSERT INTO planets 
+        (name, state, size, satellites)
+        VALUES (?, ?, ?, ?)
+    `;
+
+    con.query(sql, [name, state, size, JSON.stringify(satellites)], (err, result) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+
+        res.status(201).json({ id: result.insertId });
+    });
+
+
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Planetų serveris darbui pasiruošęs ant ${port} porto!`);
+});
