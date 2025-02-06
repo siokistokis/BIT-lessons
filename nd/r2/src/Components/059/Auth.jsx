52dd063fa0fafdb40e7f99dcceae6ffdb31c4313
siokistokis <siokis2000@yahoo.com>
2025-02-06 11:18:33 +0200

    changes

--- nd/r2/src/Components/059/Auth.jsx
+++ nd/r2/src/Components/059/Auth.jsx
@@ -1,23 +1,92 @@
-import { createContext, useEffect, useState } from 'react';
+import { createContext, useEffect, useState, useContext, use } from 'react';
+import RouterContext from './Router';
+import axios from 'axios';
+import Wrapper from './Wrapper';
 
 const AuthContext = createContext();
+const URL = 'http://localhost:3333';
+
+const redirectAfterLogin = 'http://localhost:3000';
 
 
 
-export const Auth = ({children}) => {
+export const Auth = ({ children }) => {
+
+    const guardedRoutes = ['shop', 'about'];
+
+    const { page } = useContext(RouterContext);
 
     const [loginData, setLoginData] = useState(null);
+    const [authMessage, setAuthMessage] = useState(null);
+
+    const [isAuth, setIsAuth] = useState(false);
+
+    const isAuthCheck = _ => {
+        if (!guardedRoutes.includes(page)) {
+            return true;
+        } else {
+            if (isAuth) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    };
+
+    useEffect(_ => {
+        setIsAuth(false);
+        axios.get(`${URL}/isauth`, { withCredentials: true })
+            .then(res => {
+                if (res.data.auth) {
+                    setIsAuth(true);
+                } else {
+                    setIsAuth(false);
+                    window.location.replace(`http://localhost:3000/#login`);
+                }
+            })
+            .catch(err => {
+                console.log(err);
+                setIsAuth(false);
+            });
+    }, [page]);
+
 
     useEffect(_ => {
         if (loginData === null) {
             return;
         }
         console.log(loginData);
+
+        axios.post(`${URL}/login`, loginData, { withCredentials: true }) // , { withCredentials: true } dėl cookie
+            .then(res => {
+                console.log(res.data);
+                if (!res.data.success) {
+                    setAuthMessage({
+                        text: res.data.message.text,
+                        color: res.data.message.color
+                    });
+                } else {
+                    setAuthMessage({
+                        text: res.data.message.text,
+                        color: res.data.message.color
+                    });
+                    setTimeout(_ => {
+                        window.location.replace(redirectAfterLogin);
+                    }, 1000);
+                }
+            })
+            .catch(err => {
+                console.log(err);
+            });
+
     }, [loginData]);
 
     return (
-        <AuthContext.Provider value={{setLoginData}}>
-            {children}
+        <AuthContext.Provider value={{
+            setLoginData,
+            authMessage
+        }}>
+            {isAuthCheck() ? children : <Wrapper><div>Loading...</div></Wrapper>}
         </AuthContext.Provider>
     );
 
--- nd/s2/server.js
+++ nd/s2/server.js
@@ -3,13 +3,20 @@ const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const app = express();
 const cors = require('cors');
+const md5 = require('md5'); // senas būdas, dabar naudojamas bcrypt
+// cookie parser
+const cookieParser = require('cookie-parser');
 
 const port = 3333;
 app.use(bodyParser.json());
 
+app.use(cookieParser());
+
 app.use(cors(
     {
-        origin: 'http://localhost:3000'
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST'],
+        credentials: true
     }
 ));
 
@@ -26,6 +33,88 @@ con.connect(err => {
         throw err;
     }
     console.log('Prisijungta prie duomenų bazės!');
+});
+
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    const sql = `
+    SELECT * 
+    FROM users 
+    WHERE name = ? AND password = ?
+    `;
+    con.query(sql, [username, md5(password)], (err, result) => {
+        if (err) {
+            throw err;
+        }
+        if (result.length === 0) {
+            res.json({
+                success: false,
+                message: {
+                    text: 'Neteisingi prisijungimo duomenys!',
+                    color: 'crimson'
+                }
+            });
+        } else {
+            // 1 random session token
+            const token = md5(Math.random());
+            // 2 save token to db
+            const sql = `
+                INSERT INTO sessions
+                (user_id, code)
+                VALUES (?, ?)
+            `;
+            con.query(sql, [result[0].id, token], (err, result) => {
+                if (err) {
+                    throw err;
+                }
+
+                // 3 save token to client cookie
+                res.cookie('token', token, {
+                    maxAge: 1000 * 60 * 60 * 24 * 7
+                });
+
+                // 4 response
+                res.json({
+                    success: true,
+                    message: {
+                        text: 'Prisijungta sėkmingai!',
+                        color: 'green'
+                    }
+                });
+            });
+        }
+    });
+});
+
+app.get('/isauth', (req, res) => {
+
+    setTimeout(_ => {
+
+
+
+        const token = req.cookies.token || '';
+        const sql = `
+        SELECT * 
+        FROM sessions 
+        WHERE code = ?
+    `;
+        con.query(sql, [token], (err, result) => {
+            if (err) {
+                throw err;
+            }
+            if (result.length === 0) {
+                res.json({
+                    auth: false
+                });
+            } else {
+                res.json({
+                    auth: true
+                });
+            }
+        });
+
+    }, 2000);
 });
 
 
