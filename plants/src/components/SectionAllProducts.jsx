0893739d9833948be8217bddf17f12fa76727382
siokistokis <siokis2000@yahoo.com>
2025-02-12 18:59:19 +0200

    changes

--- plants/src/components/SectionAllProducts.jsx
+++ plants/src/components/SectionAllProducts.jsx
@@ -40,5 +40,5 @@ export default function SectionAllProducts(){
         <SectionHot allProducts = {allProducts}/>
  
         </>
-    )
-}
+    );
+};
--- plants/src/components/SectionHot.jsx
+++ plants/src/components/SectionHot.jsx
@@ -1,6 +1,7 @@
 import Title from "./Title";
 import "./SectionHot.css";
 import HotCardList from "./HotCardList";
+import PlantCard from "./PlantCard";
 
 
 export default function SectionHot(allProducts) {
@@ -8,7 +9,22 @@ export default function SectionHot(allProducts) {
         <div className="light-bg">
             <div className="wrapper hot-section">
             <Title text1='Hottest ' text2='Plants' showLink={true} />
-            <HotCardList allProducts={allProducts} rating={5} />
+            {/* <HotCardList allProducts={allProducts} rating={5} /> */}
+
+            <div className="hotCard-list">
+            {allProducts.map((card) => (
+                <PlantCard
+                    key={card.id}
+                    imageSrc={card.imageSrc}
+                    title={card.title}
+                    price={card.price}
+                    showDiscount={true}
+                    discount={card.discount}
+                    
+                />
+            ))}
+        </div>
+
             </div>
         </div>
         
